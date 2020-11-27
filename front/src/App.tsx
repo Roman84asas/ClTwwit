8d008fb916029aebc2a5d6eab8d078774780ac53
RomanMereneanu <asasin84@mail.ru>
2020-11-27 18:26:10 +0200

    add pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SignIn from "./pages/Signin";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <SignIn />
     </div>
   );
 }
---
+++ front/src/pages/Signin.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SignIn() {
+    return (
+        <div>
+            Sign in
+        </div>
+    )
+}
+export default SignIn;
