23eac53b4c849c9df705d1f88c525730e8040326
RomanMereneanu <asasin84@mail.ru>
2020-11-27 19:07:29 +0200

    add pages

--- front/src/pages/Signin.tsx
+++ front/src/pages/Signin.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import {makeStyles} from "@material-ui/core";
-//import theme from "../theme";
+import {makeStyles, Typography} from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
-    wrapper: {},
-    blueSide: {},
+    wrapper: {
+        display: 'flex',
+        height: '100%',
+    },
+    blueSide: {
+        backgroundColor: '#1DA1F2',
+    },
     loginSide: {},
 }));
 
@@ -13,8 +17,29 @@ function SignIn() {
     const classes = useStyles();
 
     return (
-        <div>
-            Sign in
+        <div className={classes.wrapper}>
+            <div className={classes.blueSide}>
+                <ul>
+                    <li>
+                        <Typography>
+                            Читайте о том, что Вам интересно.
+                        </Typography>
+                    </li>
+                    <li>
+                        <Typography>
+                            Узнайте, о чем говорят в мире.
+                        </Typography>
+                    </li>
+                    <li>
+                        <Typography>
+                            Присоединяйтесь к общению
+                        </Typography>
+                    </li>
+                </ul>
+            </div>
+            <div className={classes.loginSide}>
+
+            </div>
         </div>
     )
 }
