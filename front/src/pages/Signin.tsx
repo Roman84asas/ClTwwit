fd1c8a03cd49c80f0144aa76282e0026ae79eca0
RomanMereneanu <asasin84@mail.ru>
2020-11-27 18:59:27 +0200

    add pages

--- front/src/pages/Signin.tsx
+++ front/src/pages/Signin.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import {makeStyles} from "@material-ui/core";
+//import theme from "../theme";
+
+
+const useStyles = makeStyles((theme) => ({
+    wrapper: {},
+    blueSide: {},
+    loginSide: {},
+}));
 
 function SignIn() {
+    const classes = useStyles();
+
     return (
         <div>
             Sign in
