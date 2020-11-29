6738d0669253f5337208be698b12c47c167dd81d
RomanMereneanu <asasin84@mail.ru>
2020-11-29 17:04:26 +0200

    add page home

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 
 import './index.css';
@@ -13,7 +13,9 @@ import theme from "./theme";
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
-          <App />
+          <Router>
+              <App />
+          </Router>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
