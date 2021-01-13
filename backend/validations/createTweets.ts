0e7a10a3d8d48b8c23948edfa8dac479d16e3c1a
RomanMereneanu <asasin84@mail.ru>
2021-01-13 17:41:58 +0200

    add validation tweet

--- backend/validations/createTweets.ts
+++ backend/validations/createTweets.ts
@@ -1,5 +1,5 @@
 import {body} from 'express-validator';
 
 export const createTweetValidations = [
-    
+    body('text', 'Введите текст!').isString().isLength({min:2,max:280,}).withMessage('Максимальное колличество символов 280!'),
 ];
