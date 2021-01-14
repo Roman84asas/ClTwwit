9a207debf0c7d2c68fa60763c0e09bf35d0ee389
RomanMereneanu <asasin84@mail.ru>
2021-01-14 20:08:09 +0200

    add

--- backend/controllers/TweetsController.ts
+++ backend/controllers/TweetsController.ts
@@ -84,7 +84,22 @@ class TweetsController {
     //Delete Tweet
     async delete(req:express.Request, res: express.Response): Promise<void> {
         try {
-                      
+            const tweetId = req.params.id;
+            if (!isValidObjectId(tweetId)) {
+                res.status(400).send();
+                return
+            }
+
+            const tweet = await TweetModel.findOne({_id: tweetId}).exec();
+            if (!tweet) {
+                res.status(404).send();
+                return
+            }
+
+            res.json({
+                status: 'success',
+                data: tweet,
+            });      
         } catch (err) {
             res.status(500).json({
                 status: 'error',
