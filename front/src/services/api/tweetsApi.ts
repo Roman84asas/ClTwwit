88c9ce2950c60b0e9d2465dd013eef7a48b487d0
RomanMereneanu <asasin84@mail.ru>
2021-01-15 22:31:46 +0200

    add

--- front/src/services/api/tweetsApi.ts
+++ front/src/services/api/tweetsApi.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 import {Tweet, TweetsState} from "../../store/ducks/tweets/contracts/state";
 
+interface Response<T> {
+    status: string,
+    data: T
+}
+
 export const TweetsApi = {
-    async fetchTweets(): Promise<TweetsState['items']> {
+    async fetchTweets(): Promise<Response<TweetsState[]>> {
         const { data } = await axios.get('/api/tweets')
-        return  data;
+        return  data.data;
     },
-    async fetchTweetData(id: string): Promise<Tweet[]> {
-        const { data } = await axios.get('/api/tweets/' + id)
-        return data;
+    async fetchTweetData(id: string): Promise<Tweet> {
+        const { data } = await axios.get<Response<Tweet>>('/api/tweets/' + id)
+        return data.data;
     },
     async addTweet(payload: Tweet): Promise<Tweet> {
         const { data } = await axios.post('/api/tweets', payload)
--- front/src/store/ducks/tweets/selectors.ts
+++ front/src/store/ducks/tweets/selectors.ts
@@ -2,15 +2,15 @@ import {RootState} from "../../store";
 import {AddFormState, LoadingState, TweetsState} from "./contracts/state";
 import {createSelector} from "reselect";
 
-export const selectTweets = (state: RootState): TweetsState => state.tweets;
+export const selectTweetsState = (state: RootState): TweetsState => state.tweets;
 
-export const selectTweetsItems = createSelector(selectTweets, tweets => tweets.items);
+export const selectLoadingState = (state: RootState): LoadingState => selectTweetsState(state).loadingState;
 
-export const selectLoadingState = (state: RootState): LoadingState => selectTweets(state).loadingState;
-
-export const selectAddFormState = (state: RootState): AddFormState => selectTweets(state).addFormState;
+export const selectAddFormState = (state: RootState): AddFormState => selectTweetsState(state).addFormState;
 
 export const selectIsTweetsLoading = (state: RootState): boolean => selectLoadingState(state) === LoadingState.LOADING;
 
 export const selectIsTweetsLoaded = (state: RootState): boolean => selectLoadingState(state) === LoadingState.LOADED;
 
+
+export const selectTweetsItems = (state: RootState) => selectTweetsState(state).items;
