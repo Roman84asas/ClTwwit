44a6c75269b925fff89dc505aba25a00ee284daa
RomanMereneanu <asasin84@mail.ru>
2021-01-20 16:15:23 +0200

    add get login func front

--- front/src/components/Notification/useSnackbar.tsx
+++ front/src/components/Notification/useSnackbar.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import {Alert} from "@material-ui/lab";
 import {Snackbar} from "@material-ui/core";
 
+interface NotificationProps {
+    children: (callback: (text: string) => void) => React.ReactElement;
+}
 
 
-
-export const useSnackbar = (children: React.ReactElement) => {
+export const Notification: React.FC<NotificationProps> = ({children}: NotificationProps): React.ReactElement => {
     const [open, setOpen] = React.useState(false);
     const [notificationText, setNotificationText] = React.useState<string>('');
 
-    const handleOpenNotification = (text: string) => {
+    const openNotification = (text: string) => {
         setNotificationText(text);
+        setOpen(true);
     };
 
     return(
         <>
+            {children(openNotification)}
             <Snackbar open={open} autoHideDuration={6000} onClose={()=> setOpen(false)}>
                 <Alert onClose={()=> setOpen(false)} severity="success">
                     {notificationText}
