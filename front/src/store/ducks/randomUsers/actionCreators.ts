d5552c94f78e914120f9478a22c6185666b62ea5
RomanMereneanu <asasin84@mail.ru>
2021-01-26 15:47:22 +0200

    add tweeter options

--- front/src/store/ducks/randomUsers/actionCreators.ts
+++ front/src/store/ducks/randomUsers/actionCreators.ts
@@ -10,11 +10,14 @@ export interface SetUsersItemsActionInterface extends Action<UsersActionsType> {
     type: UsersActionsType.SET_ITEMS;
     payload: User[];
 }
-
+export interface FetchUsersItemsActionInterface extends Action<UsersActionsType> {
+    type: UsersActionsType.FETCH_ITEMS;
+    payload: User[];
+}
 
 export const setUsers = (payload: User[]): SetUsersItemsActionInterface => ({
     type: UsersActionsType.SET_ITEMS,
     payload,
 });
 
-export type UsersActions = SetUsersItemsActionInterface;
+export type UsersActions = SetUsersItemsActionInterface | FetchUsersItemsActionInterface;
--- front/src/store/ducks/randomUsers/reducer.ts
+++ front/src/store/ducks/randomUsers/reducer.ts
@@ -1,30 +1,25 @@
 import produce, {Draft} from "immer";
 
-import {LoadingState, TagsState} from "./contracts/state";
-import {TagsActions, TagsActionsType} from "./actionCreators";
+import {LoadingState, UsersState} from "./contracts/state";
+import {UsersActions, UsersActionsType} from "./actionCreators";
 
-const initialTagsState: TagsState = {
+const initialUsersState: UsersState = {
     items: [],
     loadingState: LoadingState.NEVER,
 };
 
-export const tagsReducer = produce((draft: Draft<TagsState>, action: TagsActions) => {
+export const usersReducer = produce((draft: Draft<UsersState>, action: UsersActions) => {
     switch (action.type) {
-        case TagsActionsType.SET_TAGS:
+        case UsersActionsType.SET_ITEMS:
             draft.items = action.payload;
             draft.loadingState = LoadingState.LOADED;
             break;
 
-        case TagsActionsType.FETCH_TAGS:
-            draft.items = [];
+        case UsersActionsType.FETCH_ITEMS:
             draft.loadingState = LoadingState.LOADING;
-            break;
-
-        case TagsActionsType.SET_LOADING_STATE:
-            draft.loadingState = action.payload;
             break;
 
         default:
             break;
     }
-}, initialTagsState);
+}, initialUsersState);
