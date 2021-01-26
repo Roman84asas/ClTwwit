3144e589c5447d0fb3c2eca7e03f9a7055b007ad
RomanMereneanu <asasin84@mail.ru>
2021-01-26 16:19:49 +0200

    add tweeter options

--- front/src/store/ducks/randomUsers/selectors.ts
+++ front/src/store/ducks/randomUsers/selectors.ts
@@ -1,4 +1,6 @@
 import {RootState} from "../../store";
-import {LoadingState, UsersState} from "./contracts/state";
+import {UsersState} from "./contracts/state";
 
 export const selectUsers = (state: RootState): UsersState => state.users;
+
+export const selectUsersItems = (state: RootState): UsersState['items'] => state.users.items;
