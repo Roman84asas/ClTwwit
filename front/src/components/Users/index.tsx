5909f85a682e1a2caa673720f54e7d8f2e27d874
RomanMereneanu <asasin84@mail.ru>
2021-01-26 20:02:58 +0200

    add tweeter options

--- front/src/components/Users/index.tsx
+++ front/src/components/Users/index.tsx
@@ -3,10 +3,13 @@ import Paper from "@material-ui/core/Paper/Paper";
 import {Avatar, Button, List, ListItem, ListItemAvatar, ListItemText, Typography} from "@material-ui/core";
 import PersonAddIcon from "@material-ui/core/SvgIcon/SvgIcon";
 import {useHomeStyles} from "../../pages/Home/theme";
+import {useSelector} from "react-redux";
+import {selectUsersItems} from "../../store/ducks/randomUsers/selectors";
 
 
 export const Users = () => {
     const classes = useHomeStyles();
+    const items = useSelector(selectUsersItems);
 
     return(
         <Paper className={classes.rightSideBlock}>
@@ -14,25 +17,30 @@ export const Users = () => {
                 <b>Кого читать</b>
             </Paper>
             <List>
-                <ListItem className={classes.rightSideBlockItem}>
-                    <ListItemAvatar>
-                        <Avatar
-                            alt="Nicolas Babin"
-                            src="https://pbs.twimg.com/profile_images/773909130352402432/XKlKwdPG_bigger.jpg"
-                        />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary="Paris"
-                        secondary={
-                            <Typography component="span" variant="body2">
-                                Твитов:3 388
-                            </Typography>
-                        }
-                    />
-                    <Button color="primary">
-                        <PersonAddIcon />
-                    </Button>
-                </ListItem>
+                {
+                    items.map((obj, key)=> (
+                        <ListItem className={classes.rightSideBlockItem}>
+                            <ListItemAvatar>
+                                <Avatar
+                                    alt="Nicolas Babin"
+                                    src="https://pbs.twimg.com/profile_images/773909130352402432/XKlKwdPG_bigger.jpg"
+                                />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Paris"
+                                secondary={
+                                    <Typography component="span" variant="body2">
+                                        Твитов:3 388
+                                    </Typography>
+                                }
+                            />
+                            <Button color="primary">
+                                <PersonAddIcon />
+                            </Button>
+                        </ListItem>
+                    ))
+                }
+
             </List>
         </Paper>
     )
