7e7682b80a0f587f98c9359cd00d7782ad52fa4c
RomanMereneanu <asasin84@mail.ru>
2021-01-29 16:19:32 +0200

    add register options

--- front/src/store/ducks/user/sagas.ts
+++ front/src/store/ducks/user/sagas.ts
@@ -1,5 +1,11 @@
 import {call, put, takeLatest} from 'redux-saga/effects'
-import {FetchSignInDataActionInterface, setUserData, setUserDataLoadingState, UserActionsType} from "./actionCreators";
+import {
+    FetchSignInDataActionInterface,
+    FetchSignUPDataActionInterface,
+    setUserData,
+    setUserDataLoadingState,
+    UserActionsType
+} from "./actionCreators";
 import {AuthApi} from "../../../services/api/authApi";
 import {LoadingState} from "./contracts/state";
 
@@ -12,7 +18,15 @@ export function* fetchSignInRequest({payload}: FetchSignInDataActionInterface) {
     } catch (e) {
         yield put(setUserDataLoadingState(LoadingState.ERROR));
     }
-
+}
+export function* fetchSignUpRequest({payload}: FetchSignUPDataActionInterface) {
+    try {
+        const {data} = yield call(AuthApi.signIn, payload);
+        yield put(setUserData(data));
+        window.localStorage.setItem('token', data.token)
+    } catch (e) {
+        yield put(setUserDataLoadingState(LoadingState.ERROR));
+    }
 }
 
 export function* userSaga() {
